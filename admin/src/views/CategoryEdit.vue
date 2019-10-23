5fd079fdab4cef872e05e64b2b3b1ea048449fed
pdeng <635639654@qq.com>
2019-10-24 01:13:24 +0800

    分类模块添加上级分类功能

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -2,6 +2,11 @@
   <div class="create-category">
     <h1>{{id ? '编辑' : '新建'}}分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="上级分类">
+        <el-select v-model="model.parent">
+          <el-option v-for="item in parents" :key="item._id" :label="item.name" :value="item._id" />
+        </el-select>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name" />
       </el-form-item>
@@ -18,15 +23,20 @@ export default {
   },
   data() {
     return {
-      model: {}
+      model: {},
+      parents: []
     }
   },
   created() {
+    this.fetchParents()
     this.id && this.fetch()
   },
   methods: {
     async fetch() {
       this.model = await this.$http.get(`/rest/categories/${this.id}`)
+    },
+    async fetchParents() {
+      this.parents = await this.$http.get(`/rest/categories`)
     },
     async save() {
       if (this.id) {
@@ -34,8 +44,9 @@ export default {
       } else {
         await this.$http.post('rest/categories', this.model)
       }
-      this.$message({
+      this.$notify({
         type: 'success',
+        title: '成功',
         message: '保存成功'
       })
       this.$router.push('/categories/list')
