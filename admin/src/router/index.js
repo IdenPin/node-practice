9b048c4fcd2d5d830a9d72ad3c63f0c40648945d
pdeng <635639654@qq.com>
2019-10-23 23:21:54 +0800

    init

---
+++ admin/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Main from "../views/Main.vue"
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: "/",
+    name: "main",
+    component: Main,
+    children: [
+      {
+        path: "categories/create",
+        component: () => import("../views/CategoryEdit.vue")
+      }
+    ]
+  }
+  // {
+  //   path: "/main",
+  //   name: "main",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ "../views/Main.vue")
+  // }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
