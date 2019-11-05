d27b23ed2179f2ca1c416db3bd34def5a3578a94
pdeng <635639654@qq.com>
2019-11-05 21:25:54 +0800

    增加 publicPath 环境区分

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -6,7 +6,5 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-
-  // eslint-disable-next-line no-undef
   publicPath: process.env.NODE_ENV === "production" ? "/HOK-Admin/" : ""
 }
